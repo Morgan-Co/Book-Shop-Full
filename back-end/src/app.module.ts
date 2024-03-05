14a08fea5b85139a6c8fd58ad068f819b173c4f7
Morgan-Co <nikitakut3000@gmail.com>
2024-03-05 22:51:02 +0300

    All

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,27 +1,27 @@
 import { Module } from '@nestjs/common';
-// import { AuthModule } from './auth/auth.module';
-// import { BookModule } from './book/book.module';
+import { AuthModule } from './auth/auth.module';
+import { BookModule } from './book/book.module';
 import { CurrencyModule } from './currency/currency.module';
 import { LanguageModule } from './language/language.module';
 import { AuthorModule } from './author/author.module';
 import { ConfigModule } from '@nestjs/config';
-// import { UserModule } from './user/user.module';
-// import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
 import { RatingModule } from './rating/rating.module';
-// import { UserBooksModule } from './user-books/user-books.module';
+import { UserBooksModule } from './user-books/user-books.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // AuthModule,
-    // BookModule,
+    AuthModule,
+    BookModule,
     CurrencyModule,
     LanguageModule,
     AuthorModule,
-    // UserModule,
-    // CategoryModule,
+    UserModule,
+    CategoryModule,
     RatingModule,
-    // UserBooksModule,
+    UserBooksModule,
   ],
 })
 export class AppModule {}
